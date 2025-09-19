477bc13e5a14505f4db64eb50120d76862284aab
William Theodorus <williamwijaya499@gmail.com>
2025-09-20 05:22:55 +0700

    0003-LONGEST-SUBSTRING-WITHOUT-REPEATING-CHARACTERS Stats: Time: 11 ms (49.93%), Space: 14.4 MB (21.95%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,11 +4,9 @@ public:
         int ans = 0, left = 0;
         unordered_set<char> strs;
         for (int right = 0; right < s.size(); right++) {
-            // shrink only if
-            if (strs.find(s[right]) != strs.end()) {
-                left += 1;
-                right = left;
-                strs.clear();
+            // keep shrinking until
+            while (strs.find(s[right]) != strs.end()) {
+                strs.erase(s[left++]);
             }
             int length = right - left + 1;
             ans = max(ans, length);
