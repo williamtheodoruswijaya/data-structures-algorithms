61ee01fb0aeda72f07ff32a1f3e4a0718b2fa30f
William Theodorus <williamwijaya499@gmail.com>
2025-10-01 18:19:08 +0700

    1019-SQUARES-OF-A-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 30.1 MB (84.88%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
-    int findPosition(vector<int>& res, int target) {
-        int left = 0, right = res.size() - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (res[mid] == target) {
-                return mid;
-            } else if (target < res[mid]) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        return left;
-    }
-
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int> res;
+        vector<int> res(nums.size(), 0);
+        int left = 0, right = nums.size() - 1;
 
-        for (int num : nums) {
-            int squaredNum = num * num;
-            
-            int pos = findPosition(res, squaredNum);
-
-            res.insert(res.begin() + pos, squaredNum);
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            if (left < right && abs(nums[left]) > abs(nums[right])) {
+                res[i] = nums[left] * nums[left];
+                left++;
+            } else {
+                res[i] = nums[right] * nums[right];
+                right--;
+            }
         }
 
         return res;
--- README.md
+++ README.md
@@ -2,7 +2,7 @@ A collection of LeetCode questions to ace the coding interviews! - Created using
 
 # My LeetCode Progress
 <!-- PROBLEM_COUNT:START -->
-88 problems solved 🚀
+88 problems solved ð
 <!-- PROBLEM_COUNT:END -->
 
 <!---LeetCode Topics Start-->
