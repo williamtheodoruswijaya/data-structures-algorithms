1360ebb0fcce66b488004713a6c5276ec4a1a1cf
William Theodorus <williamwijaya499@gmail.com>
2025-10-01 16:54:46 +0700

    0441-ARRANGING-COINS Stats: Time: 0 ms (100%), Space: 8.9 MB (35.21%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
     int arrangeCoins(int n) {
         if (n == 1) return n;
-        
-        int left = 0, right = n - 1;
+
+        int left = 1, right = n;
         while (left <= right) {
             long long int mid = left + (right - left) / 2;
-            long long int sum = (mid * (mid + 1)) / 2;
-            if (sum < n) {
-                left = mid + 1;
-            } else if (sum > n) {
+            
+            long long int totalSum = (mid * (mid + 1)) / 2;
+            
+            if (totalSum == n) {
+                return mid;
+            } else if (totalSum > n) {
                 right = mid - 1;
             } else {
-                return mid;
+                left = mid + 1;
             }
         }
 
--- README.md
+++ README.md
@@ -2,7 +2,7 @@ A collection of LeetCode questions to ace the coding interviews! - Created using
 
 # My LeetCode Progress
 <!-- PROBLEM_COUNT:START -->
-85 problems solved 🚀
+85 problems solved ð
 <!-- PROBLEM_COUNT:END -->
 
 <!---LeetCode Topics Start-->
